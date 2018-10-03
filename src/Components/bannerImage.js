8062d71aaf5ecd7b5648af3e9339b8c25bb88b9a
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-03 12:55:26 +0800

    Configured BannerImage component.

---
+++ src/Components/bannerImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BannerImage = () => {
+    return (
+        <div>
+            <img className="bannerImage" src="/Images/bannerImage.png" />
+        </div>
+    )
+}
+
+export default BannerImage;
