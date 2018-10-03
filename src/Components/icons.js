d45cccb4967b4b519e833712430e9f160c640abd
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-03 13:02:31 +0800

    Uploaded images and configured Icon component.

---
+++ src/Components/icons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Icons = () => {
+    return (
+        <div className="icons">
+            <img className="retweetIcon" src="/Images/retweetIcon.png" />
+            <img className="likeIcon" src="/Images/likeIcon.jpeg" />
+            <img className="mailIcon" src="/Images/mailIcon.png" />
+        </div>
+    )
+}
+
+export default Icons;
