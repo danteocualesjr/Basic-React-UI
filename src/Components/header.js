8ee41f3734d937b993ca5bb737a35f0ad5f2d8d1
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-03 12:32:47 +0800

    Configured Header component.

---
+++ src/Components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <Avatar />
+            <HeaderTitle />
+            <HeaderText />
+        </div>
+    )
+}
+
+export default Header;
