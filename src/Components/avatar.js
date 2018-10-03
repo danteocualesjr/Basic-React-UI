2899f92440917b81af1e3da14dc0a36f7f393641
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-03 12:41:44 +0800

    Configured Avatar component.

---
+++ src/Components/avatar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Avatar = () => {
+    return (
+        <div className="avatar">
+            <img className="avatar" src="/Images/avatar.png" />
+        </div>
+    )
+}
