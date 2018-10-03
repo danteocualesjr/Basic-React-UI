5318049f3464b72dd215aaa0046399b1b770ef2a
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-03 12:46:30 +0800

    Configured HeaderTitle component.

---
+++ src/Components/headerTitle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HeaderTitle = () => {
+    return (
+        <div className="headerTitle">
+            <h4 className="headerOne">
+                Lambda Next
+            </h4>
+            <p className="headerTwo">@LambdaSchool</p>
+            <p className="headerThree">October 3, 2018</p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
