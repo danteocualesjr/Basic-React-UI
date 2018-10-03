214273511819667214e4dc2d079152a508e72e77
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-03 12:50:03 +0800

    Configured HeaderText component.

---
+++ src/Components/headerText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderText = () => {
+    return (
+        <div>
+            <p className="headerText">Lambda Next is Lambda School's post-graduate program designed to help its alumni land Software Engineering jobs quickly. Join now!</p>
+        </div>
+    )
+}
+
+export default HeaderText;
